d4cde41aba4fe8db18d2677c9935bb06ebd34d17
leo04101995 <vuduylam3333@gmail.com>
2022-12-09 08:16:21 +0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { StoreProvider } from 'easy-peasy';
+import store from './store';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StoreProvider store={store}>
+      <Router>
+        <Route path="/" component={App} />
+      </Router>
+    </StoreProvider>
+  </React.StrictMode>
+);
